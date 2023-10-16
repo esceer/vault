27a2007b1c57de245d7d557b3c1e26a1bdd79e7f
esceer <roland.kts@gmail.com>
2023-10-17 01:43:21 +0200

    Add home page skeleton

---
+++ frontend/src/components/Welcome.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <img src="../assets/logo.svg">
+        <div class="text">
+            <h2>Welcome to Vault, your personal secret assistant!</h2>
+            <p>You can securely store passwords, pins, notes or all sorts of secrets you want to hide from the public.</p>
+            <p>Vault keeps everything <span>isolated</span> for you.</p>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.container {
+    align-items: center;
+    display: flex;
+}
+
+img {
+    width: 300px;
+}
+
+h2, span{
+    color: var(--dusky-green);
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,10 +1,56 @@
 <script setup>
-// import TheWelcome from '../components/TheWelcome.vue'
+import Welcome from '../components/Welcome.vue'
 </script>
 
 <template>
   <main>
-    <!-- <TheWelcome /> -->
-    Home
+    <div class="welcome">
+      <Welcome />
+    </div>
+    <h2>Let's get started!</h2>
+    <p>Save your secrets. You will find it here whenever you need.</p>
+    <div>
+      <button class="filled">Add secret</button>
+      <button class="framed">Search</button>
+    </div>
   </main>
 </template>
+
+<style scoped>
+main {
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+}
+
+.welcome {
+  margin: 50px;
+}
+
+h2 {
+  font-weight: bold;
+}
+
+p {
+  color: var(--vt-c-black-mute);
+}
+
+button {
+  border: 2px solid var(--dusky-green);
+  border-radius: 5px;
+  font-weight: bold;
+  margin: 5px;
+  min-width: 150px;
+  padding: 8px 10px;
+}
+
+.filled {
+  background-color: var(--dusky-green);
+  color: var(--soft-white-green-accent);
+}
+
+.framed {
+  background-color: var(--soft-white-green-accent);
+  color: var(--dusky-green);
+}
+</style>
